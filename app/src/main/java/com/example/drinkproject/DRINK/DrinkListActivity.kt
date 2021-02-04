d6698e54031ce7c4af4aa87147efc435d2b49e7b
stopone3119 <wonhye2724@naver.com>
2021-02-04 22:43:26 +0900

    RecyclerView 보기 형식 구현  - Grid  - List(Vertical)
     DrinkListActivity ImageView 클릭 리스너 구현
     (그리드형식, 리스트형식)
    
     DrinkListAdapter viewType 추가

--- app/src/main/java/com/example/drinkproject/DRINK/DrinkListActivity.kt
+++ app/src/main/java/com/example/drinkproject/DRINK/DrinkListActivity.kt
@@ -2,33 +2,55 @@ package com.example.drinkproject.DRINK
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.drinkproject.R
-import com.example.drinkproject.RESOURCE.ADAPTER.testAdapter
+import com.example.drinkproject.RESOURCE.ADAPTER.DrinkListAdapter
 import kotlinx.android.synthetic.main.activity_drink_list.*
 
-class DrinkListActivity : AppCompatActivity() {
+class DrinkListActivity : AppCompatActivity(), View.OnClickListener {
 
     var drinkType : String =""
+    val arrayList : ArrayList<String> = arrayListOf("asd", "sss", "123", "123", "123")
+    var adapter = DrinkListAdapter(this, arrayList, 1)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drink_list)
-
         init()
-
-        val arrayList : ArrayList<String> = arrayListOf("asd", "sss", "123", "123", "123")
-        val adapter = testAdapter(this, arrayList)
-
-        recyclerView.layoutManager = GridLayoutManager(this, 2)
-        recyclerView.adapter = adapter
     }
 
     fun init(){
         when(intent.hasExtra("drinkType")){
             true -> { drinkType = intent.getStringExtra("drinkType")!! }
             false -> finish() // 들어온 값이 없을때 처리
+        }
+
+        gridButton.setOnClickListener(this)
+        listButton.setOnClickListener(this)
+
+        recyclerView.layoutManager = LinearLayoutManager(this)
+        recyclerView.adapter = adapter
+    }
+
+    override fun onClick(view: View?) {
+        val listArrayList : ArrayList<String> = arrayListOf("asd", "sss", "123", "123", "123")
+        when(view){
+            gridButton -> {
+                arrayList.clear()
+                val adapter = DrinkListAdapter(this, listArrayList, 0)
+                recyclerView.layoutManager = GridLayoutManager(this, 2)
+                recyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
+            listButton -> {
+                arrayList.clear()
+                val adapter = DrinkListAdapter(this, listArrayList, 1)
+                recyclerView.layoutManager = LinearLayoutManager(this)
+                recyclerView.adapter = adapter
+                adapter.notifyDataSetChanged()
+            }
         }
     }
 }
---
+++ app/src/main/java/com/example/drinkproject/RESOURCE/ADAPTER/DrinkListAdapter.kt
@@ -0,0 +1,65 @@
+package com.example.drinkproject.RESOURCE.ADAPTER
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.isInvisible
+import androidx.recyclerview.widget.RecyclerView
+import com.example.drinkproject.R
+
+class DrinkListAdapter(val context : Context, val list : ArrayList<String>, val chooseLayout : Int) : RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    companion object {
+        const val GRID_TYPE = 0
+        const val LIST_TYPE = 1
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when(chooseLayout){
+            0 -> GRID_TYPE
+            1 -> LIST_TYPE
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when(viewType){
+            GRID_TYPE -> {
+                val gridView = LayoutInflater.from(context).inflate(R.layout.drink_grid_view, parent, false)
+                GridHolder(gridView)
+            }
+            LIST_TYPE -> {
+                val listView = LayoutInflater.from(context).inflate(R.layout.drink_list_view, parent, false)
+                ListHolder(listView)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder){
+            is GridHolder -> holder.bind()
+            is ListHolder -> holder.bind()
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    inner class GridHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        val view3 = itemView.findViewById<View>(R.id.view3)
+
+        fun bind(){
+            when(adapterPosition%2==1){
+                true -> { view3.isInvisible = true }
+            }
+        }
+    }
+
+    inner class ListHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
+        fun bind() {
+
+        }
+    }
+}
